7c67e2988ffbf0122fc88d06dfea0dcbd55dde4f
goeun-dev <goeunlee.me@gmail.com>
2019-11-24 17:58:41 +0900

    [add] 비즈니스 계층 추가 (Service 영역)
    BoardService.java - 등록, 수정, 삭제, 조회, 리스트 조회 메서드 추가
    BoardServiceImpl.java
    BoardServiceTest.java - BoardService 중입 테스트 추가
    root-context.xml - service 패키지 스캔

---
+++ src/main/java/org/go/service/BoardServiceImpl.java
@@ -0,0 +1,52 @@
+package org.go.service;
+
+import java.util.List;
+
+import org.go.domain.BoardVO;
+import org.go.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
