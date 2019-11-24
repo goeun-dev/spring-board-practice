5343558653ec43034a28fea23f07f39d80c256ca
goeun-dev <goeunlee.me@gmail.com>
2019-11-24 18:45:06 +0900

    [add] 등록, 수정, 삭제, 조회, 리스트 구현, 테스트 (비즈니스 계층)
    BoardServiceImpl.java - 구현
    BoardServiceTest.java - 테스트 추가

--- src/test/java/org/go/service/BoardServiceTests.java
+++ src/test/java/org/go/service/BoardServiceTests.java
@@ -2,6 +2,7 @@ package org.go.service;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.go.domain.BoardVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,50 @@ public class BoardServiceTests {
 
 		log.info(service);
 		assertNotNull(service);
+	}
+
+	@Test
+	public void testRegister() {
+
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+
+		service.register(board);
+
+		log.info("생성된 게시물의 번호: " + board.getBno());
+	}
+
+	@Test
+	public void testGetList() {
+
+		service.getList().forEach(board -> log.info(board));
+	}
+
+	@Test
+	public void testGet() {
+
+		log.info(service.get(1L));
+	}
+
+	@Test
+	public void testUpdate() {
+
+		BoardVO board = service.get(1L);
+
+		if (board == null) {
+			return;
+		}
+
+		board.setTitle("제목 수정합니다.");
+		log.info("MODIFY RESULT: " + service.modify(board));
+	}
+
+	@Test
+	public void testDelete() {
+
+		log.info("REMOVE RESULT: " + service.remove(2L));
+
 	}
 }
